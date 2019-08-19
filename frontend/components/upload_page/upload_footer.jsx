052822b018495c97137dc3941b44c32303708860
gabriel <gfl.devo@gmail.com>
2019-08-18 20:48:09 -0700

    Update upload_footer component to match linter styling and best practices

--- frontend/components/upload_page/upload_footer.jsx
+++ frontend/components/upload_page/upload_footer.jsx
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
-import { Link, Redirect, Route } from 'react-router-dom';
-import classNames from 'classnames';
+import React from "react";
+// import { Link, Redirect, Route } from "react-router-dom";
+// import classNames from "classnames";
 
-class UploadFooter extends Component {
-  render() {
-    return (
-      <div className="upload-footer">
-        <p>
-          By uploading, you confirm that you're providing <em>good</em> music.
-          If it's not, our music gnomes will take it down ASAP.
-        </p>
-        <small>You've been warned.</small>
-        <div className="divider">
-          <Link to="">Legal</Link> - <Link to="">Privacy</Link> - <Link to="">Cookies</Link> <br />
-          Language: English (US)
-        </div>
+const UploadFooter = props => {
+  return (
+    <div className="upload-footer">
+      <p>
+        By uploading, you confirm that you&lsquo;re providing <em>good</em>{" "}
+        music. If it&lsquo;s not, our music gnomes will take it down ASAP.
+      </p>
+      <small>You&lsquo;ve been warned.</small>
+      <div className="divider">
+        <a href="http://google.com/">Legal</a> -{" "}
+        <a href="http://google.com/">Privacy</a> -{" "}
+        <a href="http://google.com/">Cookies</a>
+        <p>Language: English (US)</p>
       </div>
-    );
-  };
+    </div>
+  );
 };
 
 export default UploadFooter;
