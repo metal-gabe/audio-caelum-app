74fa37a25d76027508e6ad95e64843d318d3d20a
neo_andres <gfl.devo@gmail.com>
2019-09-01 00:07:42 -0700

    clean up splash page and splash trending to meet eslint & prettier rules

--- frontend/components/splash_page/splash_trending.jsx
+++ frontend/components/splash_page/splash_trending.jsx
@@ -1,50 +1,33 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import SongItem from './song_item';
 
-class SplashTrending extends Component {
-  shouldComponentUpdate() {};
+const SplashTrending = props => {
+  const { playSong, toggleSessionModal, trendingSongs } = props;
+  const tsMid = Math.floor(trendingSongs.length / 2);
+  const trendGroup1 = trendingSongs.slice(0, tsMid);
+  const trendGroup2 = trendingSongs.slice(tsMid);
 
-  render() {
-    const { trendingSongs } = this.props;
-    const tsMid = Math.floor(trendingSongs.length / 2);
-    const trendGroup1 = trendingSongs.slice(0, tsMid);
-    const trendGroup2 = trendingSongs.slice(tsMid);
+  const renderTrendingGroup1 = trendGroup1.map(song => {
+    return <SongItem key={song.id} playSong={playSong} song={song} />;
+  });
+  const renderTrendingGroup2 = trendGroup2.map(song => {
+    return <SongItem key={song.id} playSong={playSong} song={song} />;
+  });
 
-    const renderTrendingGroup1 = trendGroup1.map((song) => {
-      return (
-        <SongItem key={song.id} song={song} playSong={this.props.playSong} />
-      );
-    });
-    const renderTrendingGroup2 = trendGroup2.map((song) => {
-      return (
-        <SongItem key={song.id} song={song} playSong={this.props.playSong} />
-      );
-    });
-
-    return (
-      <section className="splash-trending">
-        <h2>Hear what's trending for free in the AudioCaelum community</h2>
-        <div className="trending-songs">
-          <div className="trending-group-1">
-            {renderTrendingGroup1}
-          </div>
-          <div className="trending-group-2">
-            {renderTrendingGroup2}
-          </div>
-          <div className="explore-trending">
-            <Link
-              to=''
-              className="explore-music-btn"
-              onClick={this.props.toggleSessionModal}
-            >
-              Explore more music
-            </Link>
-          </div>
+  return (
+    <section className="splash-trending">
+      <h2>Hear what&lsquo;s trending for free in the AudioCaelum community</h2>
+      <div className="trending-songs">
+        <div className="trending-group-1">{renderTrendingGroup1}</div>
+        <div className="trending-group-2">{renderTrendingGroup2}</div>
+        <div className="explore-trending">
+          <button className="explore-music-btn" onClick={toggleSessionModal}>
+            Explore more music
+          </button>
         </div>
-      </section>
-    );
-  };
+      </div>
+    </section>
+  );
 };
 
 export default SplashTrending;
