824439466e0aacb3ff6fff650050227776ebac7f
Gabriel F. Lujan <gflujan@gmail.com>
2020-04-27 20:46:53 -0700

    update formatting for `configureStore` and `rootReducer`

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,13 +1,30 @@
+/* -------------------------------------------------------------------------- */
+/* ALL IMPORTS */
+/* -------------------------------------------------------------------------- */
+// React
+// Packages
 import { combineReducers } from 'redux';
+
+// Context
+// Components
+// Assets
+// Constants
+
+// Utils / Methods
 import EntitiesReducer from './entities_reducer';
-import SessionReducer from './session_reducer';
 import ErrorsReducer from './errors_reducer';
 import PlayingSongReducer from './playing_song_reducer';
+import SessionReducer from './session_reducer';
 
+// Styles
+
+/* -------------------------------------------------------------------------- */
+/* START OF ROOT REDUCER TO COMBINE ALL OTHER REDUCERS */
+/* -------------------------------------------------------------------------- */
 const RootReducer = combineReducers({
   entities: EntitiesReducer,
-  session: SessionReducer,
   errors: ErrorsReducer,
+  session: SessionReducer,
   ui: PlayingSongReducer,
 });
 
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,11 +1,28 @@
-import { createStore, applyMiddleware } from 'redux'; 
-import { logger } from 'redux-logger'; 
-import thunk from 'redux-thunk'; 
-import RootReducer from '../reducers/root_reducer'; 
+/* -------------------------------------------------------------------------- */
+/* ALL IMPORTS */
+/* -------------------------------------------------------------------------- */
+// React
+// Packages
+import { createStore, applyMiddleware } from 'redux';
+import { logger } from 'redux-logger';
+import thunk from 'redux-thunk';
 
-const configureStore = (preloadedState={}) => 
-  process.env.NODE_ENV === 'production' 
-    ? createStore(RootReducer, preloadedState, applyMiddleware(thunk)) 
-    : createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger)); 
+// Context
+// Components
+// Assets
+// Constants
 
-export default configureStore; 
+// Utils / Methods
+import RootReducer from '../reducers/root_reducer';
+
+// Styles
+
+/* -------------------------------------------------------------------------- */
+/* START OF STORE CONFIGURATION */
+/* -------------------------------------------------------------------------- */
+const configureStore = (preloadedState = {}) =>
+  process.env.NODE_ENV === 'production'
+    ? createStore(RootReducer, preloadedState, applyMiddleware(thunk))
+    : createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger));
+
+export default configureStore;
