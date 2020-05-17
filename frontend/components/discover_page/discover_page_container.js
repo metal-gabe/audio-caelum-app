cc50eb62386c35ad0afef2ccd70c50bbde0f061f
Gabriel F. Lujan <gflujan@gmail.com>
2020-05-16 21:44:45 -0700

    update syntax for discover page container

--- frontend/components/discover_page/discover_page_container.js
+++ frontend/components/discover_page/discover_page_container.js
@@ -1,25 +1,47 @@
+/* -------------------------------------------------------------------------- */
+/* ALL IMPORTS */
+/* -------------------------------------------------------------------------- */
+// React
+
+// Packages
 import { connect } from 'react-redux';
-import { logoutAC } from '../../actions/session_actions';
-import { requestAllUsersAC } from '../../actions/user_actions';
-import { requestAllSongsAC } from '../../actions/song_actions';
+
+// Context
+
+// Components
 import DiscoverPage from './discover_page';
 
+// Assets
+// Constants
+
+// Utils / Methods
+import { requestAllUsersAC } from '../../actions/user_actions';
+import { requestAllSongsAC } from '../../actions/song_actions';
+
+// Styles
+
+/* -------------------------------------------------------------------------- */
+/* START OF REDUX CONTAINER FOR DISCOVER PAGE */
+/* -------------------------------------------------------------------------- */
 const mapStateToProps = ({ entities, session, errors }) => {
-  return ({
+  return {
     currentUser: entities.users[session.id],
     loggedIn: Boolean(session.id),
     allUsers: entities.users,
     allSongs: Object.values(entities.songs),
-  });
+  };
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return ({
+const mapDispatchToProps = dispatch => {
+  return {
     requestAllUsers: () => dispatch(requestAllUsersAC()),
     requestAllSongs: () => dispatch(requestAllSongsAC()),
-  });
+  };
 };
 
-const DiscoverPageContainer =
-  connect(mapStateToProps, mapDispatchToProps)(DiscoverPage);
+const DiscoverPageContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DiscoverPage);
+
 export default DiscoverPageContainer;
