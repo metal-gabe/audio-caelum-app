01c89fec30f741dffc6facf9cdd3d395231c54b3
Gabriel M. Lujan <gfl.wrk@gmail.com>
2022-07-17 00:09:42 -0700

    feat(main): update styling & linting rules and apply to files;

--- frontend/util/user_api_util.js
+++ frontend/util/user_api_util.js
@@ -1,34 +1,38 @@
-export const createUserAPI = (user) => {
-  return ($.ajax({
-    method: `POST`,
-    url: `/api/users`,
-    data: user,
-    contentType: false,
-    processData: false,
-  }));
+export const createUserAPI = user => {
+   return $.ajax({
+      'method': `POST`,
+      'url': `/api/users`,
+      'data': user,
+      'contentType': false,
+      'processData': false,
+   });
 };
+
 export const fetchAllUsersAPI = () => {
-  return ($.ajax({
-    method: `GET`,
-    url: `/api/users`,
-  }));
+   return $.ajax({
+      'method': `GET`,
+      'url': `/api/users`,
+   });
 };
-export const fetchUserAPI = (id) => {
-  return ($.ajax({
-    method: `GET`,
-    url: `/api/users/${id}`,
-  }));
+
+export const fetchUserAPI = id => {
+   return $.ajax({
+      'method': `GET`,
+      'url': `/api/users/${id}`,
+   });
 };
+
 // Still need to add the below update action in my controller
-export const updateUserAPI = (id) => {
-  return ($.ajax({
-    method: `PATCH`,
-    url: `/api/users/${id}`,
-  }));
+export const updateUserAPI = id => {
+   return $.ajax({
+      'method': `PATCH`,
+      'url': `/api/users/${id}`,
+   });
 };
-export const deleteUserAPI = (id) => {
-  return ($.ajax({
-    method: `DELETE`,
-    url: `/api/users/${id}`,
-  }));
+
+export const deleteUserAPI = id => {
+   return $.ajax({
+      'method': `DELETE`,
+      'url': `/api/users/${id}`,
+   });
 };
