7913e793bf20cc30fe6ea8e0c1f14e3901ace5ac
Gabriel M. Lujan <gfl.wrk@gmail.com>
2022-07-19 20:53:19 -0700

    feat(main): update syntax formatting;

--- db/schema.rb
+++ db/schema.rb
@@ -12,49 +12,49 @@
 
 ActiveRecord::Schema.define(version: 2019_03_07_012015) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+   # These are extensions that must be enabled in order to support this database
+   enable_extension "plpgsql"
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
+   create_table "active_storage_attachments", force: :cascade do |t|
+      t.string "name", null: false
+      t.string "record_type", null: false
+      t.bigint "record_id", null: false
+      t.bigint "blob_id", null: false
+      t.datetime "created_at", null: false
+      t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+      t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+   end
 
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
+   create_table "active_storage_blobs", force: :cascade do |t|
+      t.string "key", null: false
+      t.string "filename", null: false
+      t.string "content_type"
+      t.text "metadata"
+      t.bigint "byte_size", null: false
+      t.string "checksum", null: false
+      t.datetime "created_at", null: false
+      t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+   end
 
-  create_table "songs", force: :cascade do |t|
-    t.string "song_title", null: false
-    t.integer "artist_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["song_title"], name: "index_songs_on_song_title", unique: true
-  end
+   create_table "songs", force: :cascade do |t|
+      t.string "song_title", null: false
+      t.integer "artist_id", null: false
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["song_title"], name: "index_songs_on_song_title", unique: true
+   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email", null: false
-    t.string "password_digest", null: false
-    t.string "session_token", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["password_digest"], name: "index_users_on_password_digest", unique: true
-    t.index ["session_token"], name: "index_users_on_session_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
-  end
+   create_table "users", force: :cascade do |t|
+      t.string "username"
+      t.string "email", null: false
+      t.string "password_digest", null: false
+      t.string "session_token", null: false
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["password_digest"], name: "index_users_on_password_digest", unique: true
+      t.index ["session_token"], name: "index_users_on_session_token", unique: true
+      t.index ["username"], name: "index_users_on_username", unique: true
+   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
